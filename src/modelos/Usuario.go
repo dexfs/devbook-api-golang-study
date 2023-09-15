77702fa36d4ae25e38ee572583bd1110ea54bd92
Andre Santos <andre@dexdev.com.br>
2023-09-14 21:28:30 -0300

    feat: add check email

--- src/modelos/Usuario.go
+++ src/modelos/Usuario.go
@@ -2,6 +2,7 @@ package modelos
 
 import (
 	"errors"
+	"github.com/badoux/checkmail"
 	"strings"
 	"time"
 )
@@ -27,6 +28,11 @@ func (usuario *Usuario) validar(etapa string) error {
 	if usuario.Email == "" {
 		return errors.New("O email é obrigatório e não pode estar em branco")
 	}
+
+	if erro := checkmail.ValidateFormat(usuario.Email); erro != nil {
+		return errors.New("O email inserido é inválido")
+	}
+
 	if etapa == "cadastro" && usuario.Senha == "" {
 		return errors.New("O senha é obrigatório e não pode estar em branco")
 	}
