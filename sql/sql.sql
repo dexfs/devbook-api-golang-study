3a902b2a69a90acb31e14438f20cc1a2211baf07
Andre Santos <andre@dexdev.com.br>
2023-09-21 20:29:09 -0300

    chore: criação ta bela publiacoes

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,12 +1,15 @@
 CREATE DATABASE IF NOT EXISTS devbook;
 USE devbook;
+DROP TABLE IF EXISTS publicacoes;
+DROP TABLE IF EXISTS seguidores;
 DROP TABLE IF EXISTS usuarios;
+
 CREATE TABLE usuarios (
     id int auto_increment primary key,
     nome varchar(50) not null,
     nick varchar(50) not null unique,
     email varchar(50) not null unique,
-    senha varchar(100) not null unique,
+    senha varchar(100) not null,
     criadoEm timestamp default current_timestamp()
 ) ENGINE=InnoDB;
 
@@ -19,3 +22,14 @@ CREATE TABLE seguidores (
 
     primary key (usuario_id, seguidor_id)
 ) ENGINE=InnoDB;
+
+
+CREATE TABLE publicacoes (
+    id        int auto_increment primary key,
+    titulo    varchar(50) not null,
+    conteudo  text        not null,
+    author_id int         not null,
+    foreign key (author_id) references usuarios (id) on delete cascade,
+    curtidas  int       default 0,
+    criadoEm  timestamp default current_timestamp()
+)
